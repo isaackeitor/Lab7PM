437610aa18531576fdac0bca148b864f627ea342
isaackeitor <josuem81010@gmail.com>
2022-09-17 23:50:46 -0600

    Corrección de error en pokemonId

--- uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/view/DetailFragment.kt
+++ uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/view/DetailFragment.kt
@@ -13,15 +13,15 @@ import gt.uvg.pokelist.repository.PokemonRepository
 
 class DetailFragment : Fragment() {
     companion object{
-        val PokemonID = "pokemonID"
+        val PokemonID = "pokemonId"
     }
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
-    private var pokemonID: Int = 0
+    private var pokemonId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let{pokemonID = it.getInt(PokemonID)}
+        arguments?.let{pokemonId = it.getInt(PokemonID)}
     }
 
     override fun onCreateView(
@@ -36,15 +36,15 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val pokemonList = PokemonRepository().getPokemonList()
-        val character = pokemonList.find{it.id == pokemonID}
+        val character = pokemonList.find{it.id == pokemonId}
         binding.textView.text= "Front"
-        Picasso.get().load(character?.imageUrlFront).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView2)
+        Picasso.get().load(character!!.imageUrlFront).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView2)
         binding.textView2.text= "Back"
-        Picasso.get().load(character?.imageUrlBack).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView3)
+        Picasso.get().load(character!!.imageUrlBack).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView3)
         binding.textView3.text= "Front Shiny"
-        Picasso.get().load(character?.imageUrlShinnyFront).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView4)
+        Picasso.get().load(character!!.imageUrlShinnyFront).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView4)
         binding.textView4.text= "Back Shiny"
-        Picasso.get().load(character?.imageUrlShinnyBack).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView5)
+        Picasso.get().load(character!!.imageUrlShinnyBack).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(binding.imageView5)
     }
 
     override fun onDestroyView() {
